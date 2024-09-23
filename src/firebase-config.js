6914df095218336a973003c0ae81a796eb2f428b
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-23 10:12:01 +0200

    commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,8 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 
-// Your web app's Firebase configuration sdk
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGV8GRK_yPee5qbDyew1Krepie1cYuZh8",
+  authDomain: "marzano-automotive.firebaseapp.com",
+  projectId: "marzano-automotive",
+  storageBucket: "marzano-automotive.appspot.com",
+  messagingSenderId: "248793604751",
+  appId: "1:248793604751:web:befdff8174575a897146fa",
+  measurementId: "G-XWVMVYBQLT"
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
