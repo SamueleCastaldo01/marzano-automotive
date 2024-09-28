b804f032d1c792cd8f9af461ac92e54e31188f92
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-28 12:00:37 +0200

    commit

--- src/pages/SchedeDiLavoro.js
+++ src/pages/SchedeDiLavoro.js
@@ -236,7 +236,7 @@ export function SchedeDiLavoro() {
           <ThemeProvider theme={theme}>
             <Paper
               className="mt-4"
-              sx={{ height: 500, borderRadius: "8px", overflowX: "auto" }}
+              sx={{ height: 450, borderRadius: "8px", overflowX: "auto" }}
             >
               <StyledDataGrid
                 rows={workCards}
